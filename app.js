a34a012fc0a26ff142e1ebeabb289771ce17281c
fady <32818953+FadyDMK@users.noreply.github.com>
2024-07-26 14:14:52 +0200

    added error handling

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/",router);
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
