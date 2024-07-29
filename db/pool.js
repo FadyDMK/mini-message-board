b0a9135c6d7d0b47a6e59a6dd93eb780464cf577
fady <32818953+FadyDMK@users.noreply.github.com>
2024-07-29 23:29:12 +0200

    modified some stuff

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
+  host: process.env.HOST, // or wherever the db is hosted
   user: process.env.ROLE,
   database: process.env.DB_NAME,
   password: process.env.ROLE_PWD,
